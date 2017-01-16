f2e45943e32292e6d582cf68fa15a10eeecb7944
Bhaskar Karambelkar <bhaskarvk@gmail.com>
2017-01-15 19:15:03 -0500

    Solved the selfcontained issue using a chunk option

--- R/knitr_support.R
+++ R/knitr_support.R
@@ -3,32 +3,29 @@ knit_print.widgetframe <- function(x, ..., options = NULL) {
   # Knit child widget
   childWidget <- attr(x$x,'widget')
 
-  #outputDir <- getwd()
-
   # Use the chunk label if available for placing the enclosed widget's HTML
-  if(!is.null(options) && !is.null(options$label)) {
+  if (!is.null(options) && !is.null(options$label)) {
     # TODO We may need to sanitize options$label
     childFile <- paste0('widget_',options$label,'.html')
   } else {
     childFile <- paste0(basename(tempfile('widget_', '.')),'.html')
   }
 
-  #childDir <- file.path(outputDir,'widgets')
-  #dir.create(childDir,recursive = TRUE, showWarnings = FALSE)
-  #setwd(childDir)
-
-  if(file.exists(childFile)) {
+  if (file.exists(childFile)) {
     unlink(childFile, force = TRUE)
   }
-  # For now the child widget is selfcontained, i.e. one big HTML with JS/CSS as data URIs.
-  # TODO figure out if we can read self_contained argument of the Rmd file and
-  # change behavior accordingly.
-  htmlwidgets::saveWidget(childWidget, childFile, libdir = 'widget_libs',
-                          knitrOptions = options)
-  #setwd(outputDir)
 
-  # Set the relative URL for child HTML
-  #x$x$url <- paste(basename(childDir),childFile, sep='/')
+  selfcontained <- FALSE
+
+  if (!is.null(options) && !is.null(options$self_contained_widgetframes) &&
+     options$self_contained_widgetframes == TRUE) {
+    selfcontained <- TRUE
+  }
+
+  htmlwidgets::saveWidget(childWidget, childFile, libdir = 'widget_libs',
+                          knitrOptions = options,
+                          selfcontained = selfcontained)
+
   x$x$url <- childFile
 
   # Knit parent widget
